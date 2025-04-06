1abc49ac0152dd4f39720f858ea106fd7680e9e2
TitanWasHere <andrea.gravili8@gmail.com>
2025-04-06 18:03:40 +0200

    finished homework 2

--- homework_2/project2.js
+++ homework_2/project2.js
@@ -1,9 +1,52 @@
-// Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
-// The transformation first applies scale, then rotation, and finally translation.
-// The given rotation value is in degrees.
+
+
+function multiplyMatrices(A, B) {
+	let result = [];
+
+	for (let i = 0; i < 3; i++) {
+		result[i] = [];
+		for (let j = 0; j < 3; j++) {
+			result[i][j] = 0;
+			for (let k = 0; k < 3; k++) {
+				result[i][j] += A[i][k] * B[k][j];
+			}
+		}
+	}
+
+	return result;
+}
+
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	const rotationRad = rotation * Math.PI / 180.0;
+	const cosTheta = Math.cos(rotationRad);
+	const sinTheta = Math.sin(rotationRad);
+
+	let scaleMatrix = [
+		[scale, 0, 0],
+		[0, scale, 0],
+		[0, 0, 1]
+	];
+	let rotationMatrix = [
+		[cosTheta, -sinTheta, 0],
+		[sinTheta, cosTheta, 0],
+		[0, 0, 1]
+	];
+	let translationMatrix = [
+		[1, 0, positionX],
+		[0, 1, positionY],
+		[0, 0, 1]
+	];
+
+	let rotationScaleMatrix = multiplyMatrices(rotationMatrix, scaleMatrix);
+	let finalMatrix = multiplyMatrices(translationMatrix, rotationScaleMatrix);
+
+	return Array (
+		finalMatrix[0][0], finalMatrix[1][0], finalMatrix[2][0],
+		finalMatrix[0][1], finalMatrix[1][1], finalMatrix[2][1],
+		finalMatrix[0][2], finalMatrix[1][2], finalMatrix[2][2]
+	);
+
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -11,5 +54,19 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+
+	return Array (
+        trans2[0] * trans1[0] + trans2[3] * trans1[1] + trans2[6] * trans1[2],
+        trans2[1] * trans1[0] + trans2[4] * trans1[1] + trans2[7] * trans1[2],
+        trans2[2] * trans1[0] + trans2[5] * trans1[1] + trans2[8] * trans1[2],
+        
+        trans2[0] * trans1[3] + trans2[3] * trans1[4] + trans2[6] * trans1[5],
+        trans2[1] * trans1[3] + trans2[4] * trans1[4] + trans2[7] * trans1[5],
+        trans2[2] * trans1[3] + trans2[5] * trans1[4] + trans2[8] * trans1[5],
+        
+        trans2[0] * trans1[6] + trans2[3] * trans1[7] + trans2[6] * trans1[8],
+        trans2[1] * trans1[6] + trans2[4] * trans1[7] + trans2[7] * trans1[8],
+        trans2[2] * trans1[6] + trans2[5] * trans1[7] + trans2[8] * trans1[8]
+	);
+
 }
