0de63e5bec363bad80ef92bdaa77fb404e0cf98e
TitanWasHere <andrea.gravili8@gmail.com>
2025-07-12 20:10:42 +0200

    [EDIT+REMOVE] Edited useless notations, moved the css into its file, remove unused constant file

--- DungeonSpirit/src/managers/ControlsManager.js
+++ DungeonSpirit/src/managers/ControlsManager.js
@@ -6,10 +6,10 @@ export class ControlsManager {
         this.camera = camera;
         this.canvas = canvas;
         this.controls = new OrbitControls(this.camera, this.canvas);
-        this.controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+        this.controls.enableDamping = true; 
         this.controls.dampingFactor = 0.25;
         this.controls.screenSpacePanning = false;
-        this.controls.maxPolarAngle = Math.PI / 2; // Limit vertical rotation
+        this.controls.maxPolarAngle = Math.PI / 2; 
     }
 
     update() {
