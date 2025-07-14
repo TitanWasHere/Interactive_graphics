ee0fdbcd14db34eae1ee6e3c90912fb6dad0998d
TitanWasHere <andrea.gravili8@gmail.com>
2025-07-14 22:43:30 +0200

    [EDIT] Fallback response of the purple spirit

--- DungeonSpirit/src/objects/NPCs/PurpleSpirit.js
+++ DungeonSpirit/src/objects/NPCs/PurpleSpirit.js
@@ -40,43 +40,36 @@ export class PurpleSpirit extends Spirit {
     }
 
     onNPCInteract(resp) {
-        // If the key was already given
         if(this.swordGiven){
             return "I already gave you the key, go to the beginning and you'll understand";
         }
         
-        // First interaction without sword
         if(!resp && this.firstInteraction && !this.swordGiven){
             this.firstInteraction = false;
             return "I need a sword, bring it to me and I will reward you";
         }
         
-        // Follow-up question after first interaction
         if(!resp && this.firstInteraction){
             return "Maybe in some kind of treasures you can find a sword..."
         }
         
-        // Player brings the sword after being asked
         if(resp && this.firstInteraction){
             this.firstInteraction = false;
             this.swordGiven = true;
             return "How did you know I needed a sword? Anyway, take this key as a reward";
         }
         
-        // Player brings the sword without being asked but after interacting once
         if(resp && this.firstInteractionAfterSword && !this.swordGiven){
             this.firstInteractionAfterSword = false;
             this.swordGiven = true;
             return "Thanks, take this key, I think you need it";
         }
         
-        // Default case when player brings sword
         if(resp && !this.swordGiven){
             this.swordGiven = true;
             return "Thank you for the sword, take this key as a reward";
         }
         
-        // Fallback response
-        return "Go to the beginning, and you'll understand";
+        return "In the corridor you will find a locked door...";
     }
 }
